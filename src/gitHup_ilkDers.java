f0f049a60784eb900ae7be4023173bb799b8a034
burcubozkurt <burcubozkurt1294@gmail.com>
2023-04-03 18:03:52 +0300

    230327

--- src/gitHup_ilkDers.java
+++ src/gitHup_ilkDers.java
@@ -3,6 +3,8 @@ public class gitHup_ilkDers {
     public static void main(String[] args) {
 
         System.out.println("Hello GitHup");
+        System.out.println("===============");
+        System.out.println("have a wise day");
 
     }
 }
